714c5c3b59a457ae6f0d8f3c2f527c7f325837ca
aranza001 <cruzcolinaranza@gmail.com>
2025-05-25 22:17:57 -0600

    Add files via upload

---
+++ referenciacion.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+int x=17,y;
+int *p;
+p=&x;
+printf ("el valor de x es: %d\n",*p);
+ y=*p+3;
+ printf("el valor de y es: %d",y); 
+ return 0;
+}
---
+++ saber cuanto ocupa de bytes.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+char cadena [10];
+printf ("un int ocupa %d\n bytes",sizeof (int));
+printf ("un char ocupa %d\n bytes",sizeof (char));
+printf ("un float ocupa %d\n bytes",sizeof (float));
+printf ("un double ocupa %d\n bytes",sizeof (double));
+printf ("cadena ocupa %d\n bytes",sizeof (cadena));
+
+}
+
+
+   
